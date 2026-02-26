7d0f84cc0f86addddc937951054418e92fd443c1
black-incendium <cryingincendium@gmail.com>
2026-02-26 12:46:56 +0100

    adjusted code to the new angine

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ import { gameConfig } from '../configs/gameConfig.js';
 import { layoutConfig } from '../configs/layoutConfig.js';
 import { eventsManager } from './glass/dist/managers/eventsManager.js'
 import { rendererEventsData } from './glass/dist/eventsData/rendererEventsData.js';
+import { progressorsManager } from './glass/dist/managers/progressorsManager.js';
+import { componentsManager } from './glass/dist/managers/componentsManager.js';
 
 const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl2', {stencil: true});
@@ -25,18 +27,17 @@ glass.initialize({
 function startMainFlow() {
 
     console.log(1)
-    // let progressorTest = progressorsManager.getNewProgressor({
-    //     startValue: 0,
-    //     targetValue: 3600,
-    //     duration: 100000,
-    //     updateCallback: (currentValue) => {
-    //         componentsManager.getComponentById('tile0x1').setRotation(currentValue)
-    //         componentsManager.getComponentById('tile1x1').setRotation(currentValue)
-    //         componentsManager.getComponentById('tile1x0').setRotation(currentValue)
-    //         componentsManager.getComponentById('tile0x0').setRotation(currentValue)
-    //     }
-    // });
-    
-    // // debugger
-    // progressorTest.start();
+    let progressorTest = progressorsManager.getNewProgressor({
+        startValue: 0,
+        targetValue: 3600,
+        duration: 100000,
+        updateCallback: (currentValue) => {
+            componentsManager.getComponentById('tile0x1').rotation = currentValue;
+            componentsManager.getComponentById('tile1x1').rotation = currentValue;
+            componentsManager.getComponentById('tile1x0').rotation = currentValue;
+            componentsManager.getComponentById('tile0x0').rotation = currentValue;
+        }
+    });
+
+    progressorTest.start();
 }
